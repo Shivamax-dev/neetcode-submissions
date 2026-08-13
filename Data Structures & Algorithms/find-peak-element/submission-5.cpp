f99b2eae3d65108b566cf1aa320046fa5cfaebbc
Shivam Maurya <124660245+Shivamax-dev@users.noreply.github.com>
2026-08-13 17:59:50 +0530

    Add: find-peak-element - submission-5

---
+++ Data Structures & Algorithms/find-peak-element/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int start = 0, end = n-1;  //start = low and end = high
+
+        while(start < end){
+            int guess = (start + end)/2;
+            if(nums[guess] < nums[guess + 1]){
+                // Mountain goes up to the right -> peak is strictly right of guess
+                start = guess + 1;
+            }else{   //when nums[guess] >= nums[guess + 1]
+                end = guess;
+            }
+        }
+        return start; // When start == end, start points to the peak
+    }
+};
